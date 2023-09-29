198c9741dfafe504698ed173d8a866319ea46a64
Karthik <mskarthik6@gmail.com>
2023-09-29 16:17:56 +0100

    `Music` model added.

---
+++ MusicApp/Models/Music.swift
@@ -0,0 +1,13 @@
+//
+//  Music.swift
+//  MusicApp
+//
+//  Created by Karthik Maharajan Skandarajah on 29/09/2023.
+//
+
+struct Music: Decodable {
+    let title: String
+    let artist: String
+    let album: String
+    let duration: String
+}
